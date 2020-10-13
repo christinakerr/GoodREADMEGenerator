d69ca6a7b356f5da3f53639421395150190ec214
Christi Kerr <christinakerr394@gmail.com>
2020-10-13 16:14:02 -0500

    Collects info from the user for the README. Fixes #1

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-    
+    "Enter project title: ",
+    "Enter description: ",
+    "Enter installation instructions: ",
+    "Enter usage information: ",
+    "Enter contribution guidelines: ",
+    "Enter test instructions: "
 ];
 
 // function to write README file
@@ -9,6 +16,40 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "title"
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "description"
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: questions[5],
+            name: "testing"
+        },
+    ]).then(response => {
+        console.log(response);
+    })
 
 }
 
