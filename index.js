0b97eadc0d93f38b714876645e6cbcaf0f2929f1
Christi Kerr <christinakerr394@gmail.com>
2020-10-15 14:56:47 -0500

    Moved prompt questions to array

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// array of questions for user
-const questions = [
-    "Enter project title: ",
-    "Enter description: ",
-    "Enter installation instructions: ",
-    "Enter usage information: ",
-    "Enter contribution guidelines: ",
-    "Enter test instructions: "
-];
-
 // array of license options
 
 const licenses = [
@@ -23,15 +13,65 @@ const licenses = [
     "MIT",
 ]
 
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "Enter project title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter installation instructions: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter usage information: ",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter contribution guidelines: ",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Enter test instructions: ",
+        name: "testing"
+    },
+    {
+        type: "list",
+        message: "Select a license: ",
+        name: "license",
+        choices: licenses
+    },
+    {
+        type: "input",
+        message: "GitHub username: ",
+        name: "gitHub"
+    },
+    {
+        type: "input",
+        message: "Email address: ",
+        name: "email"
+    }
+];
+
 const dir = "./output";
 
 // function to write README file
 function writeToFile(fileName, data) {
-    if (!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
     }
-    return fs.writeFile(fileName, data, function(err){
-        if (err){
+    return fs.writeFile(fileName, data, function (err) {
+        if (err) {
             console.log("Your README could not be created. Please try again.")
         } else {
             console.log("Success!")
@@ -41,58 +81,11 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: questions[0],
-            name: "title"
-        },
-        {
-            type: "input",
-            message: questions[1],
-            name: "description"
-        },
-        {
-            type: "input",
-            message: questions[2],
-            name: "installation"
-        },
-        {
-            type: "input",
-            message: questions[3],
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: questions[4],
-            name: "contribution"
-        },
-        {
-            type: "input",
-            message: questions[5],
-            name: "testing"
-        },
-        {
-            type: "list",
-            message: "Select a license: ",
-            name: "license",
-            choices: licenses
-        },
-        {
-            type: "input",
-            message: "GitHub username: ",
-            name: "gitHub"
-        },
-        {
-            type: "input",
-            message: "Email address: ",
-            name: "email"
-        }
-    ]).then(response => {
-        console.log(response);
-
-        writeToFile(path.join(dir, "README.md"), generateMarkdown({...response}));
-    })
+    inquirer.prompt(questions)
+        .then(response => {
+            console.log(response);
+            writeToFile(path.join(dir, "README.md"), generateMarkdown({ ...response }));
+        })
 
 }
 
