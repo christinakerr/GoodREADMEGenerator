c10192bbc905a0b16b630d9f9006b4b72cfa49d0
Christi Kerr <christinakerr394@gmail.com>
2020-10-15 15:47:38 -0500

    generateMarkdown.js now returns the entire README page with user input. Fixes #11

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,61 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  let licenseCode;
   switch (data.license){
     case "MIT":
-      const licenseCode = "[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)";
+      licenseCode = "[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)";
       break;
     case "GPLv3":
-      const licenseCode = "[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)";
+      licenseCode = "[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)";
       break;
     case "Creative Commons":
-      const licenseCode = "[![CC-0 license](https://img.shields.io/badge/License-CC--0-blue.svg)](https://creativecommons.org/licenses/by-nd/4.0)";
+      licenseCode = "[![CC-0 license](https://img.shields.io/badge/License-CC--0-blue.svg)](https://creativecommons.org/licenses/by-nd/4.0)";
       break;
     default:
-      const licenseCode = "";
+      licenseCode = "";
   };
   return `# ${data.title}
 
+${licenseCode}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is covered under the ${data.license} license.
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.testing}
+
+## Questions
+
+[GitHub](https://www.github.com/${data.gitHub})
+
+Contact me: ${data.email}
 `;
 }
 
